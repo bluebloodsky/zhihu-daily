5cfa172c06ab29bc02392ef2bc8e0bcf19df913b
bluebloodsky <zhangliang1101@126.com>
2018-08-07 11:49:47 +0800

    修改section bug

--- app/src/main/java/com/hust/zl/daily/SectionActivity.java
+++ app/src/main/java/com/hust/zl/daily/SectionActivity.java
@@ -2,14 +2,15 @@ package com.hust.zl.daily;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hust.zl.daily.gson.SectionNews;
-import com.hust.zl.daily.gson.SectionStory;
 import com.hust.zl.daily.other.LoadMoreScrollListener;
-import com.hust.zl.daily.other.RefreshLayout;
 import com.hust.zl.daily.util.HttpUtil;
 import com.hust.zl.daily.util.Utility;
 
@@ -24,38 +25,84 @@ import okhttp3.Response;
 public class SectionActivity extends Activity {
 
     private Button backButton;
-    private RecyclerView rv_stories;
-    List<Object> sectionStoryList;
-    StoryAdapter adapter;
+    private TextView titleTextView;
+    private RecyclerView recyclerViewStories;
+    private List<Object> sectionStoryList;
+    private StoryAdapter adapter;
     private String sectionId;
+    private String timeStamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section);
         backButton = (Button) findViewById(R.id.back_button);
-        rv_stories = (RecyclerView) findViewById(R.id.rv_stories);
+        recyclerViewStories = (RecyclerView) findViewById(R.id.rv_stories);
+
+        titleTextView = (TextView) findViewById(R.id.section_name);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerViewStories.setLayoutManager(linearLayoutManager);
 
         sectionStoryList = new ArrayList<>(0);
-        StoryAdapter adapter = new StoryAdapter(sectionStoryList);
+        adapter = new StoryAdapter(sectionStoryList);
 
-        rv_stories.setAdapter(adapter);
-        rv_stories.addOnScrollListener(new LoadMoreScrollListener() {
+        recyclerViewStories.setAdapter(adapter);
+        recyclerViewStories.addOnScrollListener(new LoadMoreScrollListener() {
             @Override
             public void onLoadMore() {
                 requestbeforeSectionStories(this);
             }
         });
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
         sectionId = getIntent().getStringExtra("section_id");
-//        requestSectionStories();
+        requestSectionStories();
     }
 
     private void requestbeforeSectionStories(final LoadMoreScrollListener listener) {
+        String storyUrl = "https://news-at.zhihu.com/api/4/section/" + sectionId + "/before/" + timeStamp;
+        HttpUtil.sendOkHttpRequest(storyUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(SectionActivity.this, "请求失败，请检查网络连接", Toast.LENGTH_SHORT).show();
+                        if (listener != null) {
+                            listener.LoadFinish();
+                        }
+                    }
+                });
+            }
 
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final SectionNews storyNews = Utility.handleSectionNews(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (storyNews != null) {
+                            timeStamp = storyNews.timestamp;
+                            sectionStoryList.addAll(storyNews.sectionStories);
+                            adapter.notifyDataSetChanged();
+                        }
+                        if (listener != null) {
+                            listener.LoadFinish();
+                        }
+                    }
+                });
+            }
+        });
     }
 
-    private void requestSectionStories(){
+    private void requestSectionStories() {
 
         String storyUrl = "https://news-at.zhihu.com/api/4/section/" + sectionId;
         HttpUtil.sendOkHttpRequest(storyUrl, new Callback() {
@@ -78,6 +125,8 @@ public class SectionActivity extends Activity {
                     @Override
                     public void run() {
                         if (storyNews != null) {
+                            titleTextView.setText(storyNews.name);
+                            timeStamp = storyNews.timestamp;
                             sectionStoryList.addAll(storyNews.sectionStories);
                             adapter.notifyDataSetChanged();
                         }
